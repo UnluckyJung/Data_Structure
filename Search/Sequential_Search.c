7ca0335e49526cf6216e72802b23bc1712e5b28d
UnluckyJung <ybook2012@gmail.com>
2019-06-26 20:03:09 +0900

    Seq_search

---
+++ Search/Sequential_Search.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+void search(int arr[], int key) {
+	
+	for (int i = 0; i < 10; ++i) {
+		if (arr[i] == key) {
+			printf("Find %d !\n", key);
+			return;
+		}
+	}
+	printf("I can't Find %d", key);
+	return;
+}
+
+int main() {
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	
+	search(arr, 3);
+	search(arr, 11);
+
+	return 0;
+}
