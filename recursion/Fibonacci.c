57c849fcf5a084923d830e111f5adc112f1e8248
UnluckyJung <ybook2012@gmail.com>
2019-07-08 17:37:17 +0900

    Fibonacci

---
+++ recursion/Fibonacci.c
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+ 
+int fibonacci(int n) {
+    if(n == 0) 
+        return 0;
+    else if(n == 1) 
+        return 1;
+ 
+    int n1 = 0;
+    int n2 = 1;
+    int result = 0;
+ 
+ 
+    for (int i = 2; i <= n; ++i) {
+        result = n1 + n2;
+        n1 = n2;
+        n2 = result;
+    }
+ 
+    return result;
+}
+ 
+int main() {
+    //0 1 1 2 3 5 [8] 13
+    printf("%d", fibonacci(6));
+ 
+    return 0;
+}
