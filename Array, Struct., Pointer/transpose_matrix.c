52d8b89a6a20d2227208489cc36e1e660422c01a
UnluckyJung <ybook2012@gmail.com>
2019-07-09 17:21:35 +0900

    transpose_matrix

---
+++ Array, Struct., Pointer/transpose_matrix.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void printf_arr(int arr[3][3]) {
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void trans(int arr[3][3]) {
+	int temp;
+	for (int i = 0; i < 3; ++i) {
+		for (int j = i; j < 3; ++j) {
+			temp = arr[i][j];
+			arr[i][j] = arr[j][i];
+			arr[j][i] = temp;
+		}
+	}
+}
+
+
+int main() {
+
+	int arr[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+	printf_arr(arr);
+
+	printf("\n\n");
+
+	trans(arr);
+	printf_arr(arr);
+	return 0;
+}
