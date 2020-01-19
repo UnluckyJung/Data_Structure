1f9ff6e38b31f45386f6f09487c64001ec964a39
JungYoonSung <ybook2012@gmail.com>
2020-01-19 18:17:03 +0900

    Quick_sort

--- Quick_sort.c
+++ Quick_sort.c
@@ -1,34 +1,35 @@
 #include<stdio.h>
-
 int arr[] = {4,1,32,4,5,6};
 
-void swap(int *a, int *b){
+void swap(int* a, int* b) {
     int tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-int partition(int s, int e){
+int partition(int s, int e) {
     int pivot = arr[e];
-    int target = s-1;
-    for(int left = s; left<e; ++left){
-        if(arr[left] <= pivot)  swap(&arr[++target], &arr[left]);
+    int target = s;
+    for (int left = s; left < e; ++left) {
+        if (arr[left] <= pivot)  swap(&arr[target++], &arr[left]);
     }
-    swap(&arr[++target], &arr[e]);
+    swap(&arr[target], &arr[e]);
     return target;
 }
 
-void q_sort(int s, int e){
-    if(s < e){
-    int q = partition(s,e);
-    q_sort(s, q-1);
-    q_sort(q+1, e);
+void q_sort(int s, int e) {
+    if (s < e) {
+        int q = partition(s, e);
+        q_sort(s, q - 1);
+        q_sort(q + 1, e);
     }
 }
-int main(){
-    for(int i = 0 ; i<6; ++i)   printf("%d ", arr[i]);
+int main() {
+    int n = sizeof(arr) / sizeof(int);
+
+    for (int i = 0; i < n; ++i)   printf("%d ", arr[i]);
     printf("\n");
-    q_sort(0, 5);
-    for(int i = 0 ; i<6; ++i)   printf("%d ", arr[i]);
+    q_sort(0, n - 1);
+    for (int i = 0; i < n; ++i)   printf("%d ", arr[i]);
     return 0;
 }
